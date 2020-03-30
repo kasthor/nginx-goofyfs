d49826c54be48a91bde4c3bdded4c0e5b5610f5b
Giancarlo Palavicini <kasthor@gmail.com>
2020-03-30 17:58:19 -0600

    feat: Verify that variables has been set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-goofys -o allow_other --uid `id -u nginx` $BUCKET_NAME /var/www
+if [ -z $BUCKET_NAME ] || [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ] 
+then
+  echo 'Environment Variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and BUCKET_NAME must be set';
+  exit 1;
+fi
 
+goofys -o allow_other --uid `id -u nginx` $BUCKET_NAME /var/www
 
 nginx -g 'daemon off;'
